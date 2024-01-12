e229d909db6f07c120a3b37ff7aca1e7f8891fa8
Lucas Pradi <lucaspradi1@gmail.com>
2024-01-12 14:21:52 +0100

    Resolving the unzipping issue.

--- gnps_postprocessing/gnps_download_results.py
+++ gnps_postprocessing/gnps_download_results.py
@@ -4,6 +4,7 @@ import zipfile
 import os
 import pandas as pd
 import requests
+import shutil
 
 def gnps_download_results(job_id, output_folder, force_redownload='yes'):
     # This function download GNPS molecular networking job results locally 
@@ -31,7 +32,7 @@ def gnps_download_results(job_id, output_folder, force_redownload='yes'):
             except OSError as e:
                 print(f"Error deleting file: {output_zip}. Error: {e}")
 
-        # Download the file using requests
+    # Download the file using requests
     try:
         with requests.post(gnps_download_link, data={}, stream=True) as r:  # Changed to POST request
             r.raise_for_status()
@@ -44,27 +45,28 @@ def gnps_download_results(job_id, output_folder, force_redownload='yes'):
     except Exception as e:
         print(f"Error in downloading file: {e}")
     
-        # Check if the download was successful
-        if not os.path.exists(output_zip):
-            print("==========> ERROR in the download -> check the job ID and/or job type")
+    # Check if the download was successful
+    if not os.path.exists(output_zip):
+        print("==========> ERROR in the download -> check the job ID and/or job type")
         
-        # Check the size of the downloaded file
+    # Check the size of the downloaded file
+    try:
+        file_size = os.path.getsize(output_zip)
+        if file_size > 10000:
+            print(f'GNPS job results were successfully downloaded as: {output_zip}')
+        else:
+            print('==========> ERROR in the download -> check the job ID and/or job type')
+    except OSError as e:
+        print(f'Error checking file size: {e}')
+
+    # Extracting the downloaded zipfile into a folder
+    if os.path.exists(output_folder):
         try:
-            file_size = os.path.getsize(output_zip)
-            if file_size > 10000:
-                print(f'GNPS job results were successfully downloaded as: {output_zip}')
-            else:
-                print('==========> ERROR in the download -> check the job ID and/or job type')
-        except OSError as e:
-            print(f'Error checking file size: {e}')
-
-        # Extracting the downloaded zipfile into a folder
-        if os.path.exists(output_folder):
-            try:
-                subprocess.call(shlex.split(f'rm -r {output_folder}'))
-            except subprocess.CalledProcessError as e:
-                print(f"Error removing existing folder: {e}")
-
+            shutil.rmtree(output_folder)
+        except Exception as e:
+            print(f"Error removing existing folder: {e}")
+    else:
+        # Attempt to unzip the file
         try:
             with zipfile.ZipFile(output_zip, "r") as zip_ref:
                 zip_ref.extractall(output_folder)
@@ -72,10 +74,11 @@ def gnps_download_results(job_id, output_folder, force_redownload='yes'):
             print("Error: The downloaded file is not a valid ZIP file.")
         except FileNotFoundError:
             print(f"Error: The file {output_zip} does not exist.")
-
-        # Check if the extraction was successful
-        if os.path.isdir(output_folder):
-            print(f'GNPS job results were successfully extracted into the folder: {output_folder}')
+        except Exception as e:
+            print(f"Unexpected error during extraction: {e}")
+        # Check if files were successfully extracted
+        if any(os.scandir(output_folder)):
+            print(f'Files were successfully extracted into the folder: {output_folder}')
         else:
             print('==========> ERROR in the extraction process')
         
