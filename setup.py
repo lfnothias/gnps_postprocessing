08009f4c02249f6429f17c636af00c587710ad8a
Lucas Pradi <155846495+lucaspradi@users.noreply.github.com>
2024-01-18 12:01:44 +0100

    Update setup.py
    Change from 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gnps_postprocessing',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='GNPS Postprocessing Scripts',
     url='https://github.com/lfnothias/gnps_postprocessing',
